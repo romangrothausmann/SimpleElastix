5420cd8887d9abc85a2fd1ca6c848de84b3f0670
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2018-06-30 10:50:48 +0200

    Elastix @ 6e031631 (master)

--- SuperBuild/External_Elastix.cmake
+++ SuperBuild/External_Elastix.cmake
@@ -2,7 +2,7 @@ set( proj Elastix )
 file( WRITE "${CMAKE_CURRENT_BINARY_DIR}/${proj}-build/CMakeCacheInit.txt" "${ep_common_cache}" )
 
 set( ELASTIX_GIT_REPOSITORY ${git_protocol}://github.com/SuperElastix/elastix )
-set( ELASTIX_GIT_TAG 6472fc81721725d37eb0f147aeda635ce42a3bb4 )
+set( ELASTIX_GIT_TAG 6e0316319a0542942f60e4d1bab06a53d3c75fb0 )
 
 ExternalProject_Add( ${proj}
   GIT_REPOSITORY ${ELASTIX_GIT_REPOSITORY}
